b54d24229dbdaa0dd81458d4aa505cefc1f7b418
TamBill <tambill.pham.ngoc@gmail.com>
2021-06-14 22:38:14 +0700

    Update string utils

--- src/main/java/com/cuongnn/tutoringappserver/common/utils/Strings.java
+++ src/main/java/com/cuongnn/tutoringappserver/common/utils/Strings.java
@@ -3,13 +3,18 @@ package com.cuongnn.tutoringappserver.common.utils;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.Optional;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Strings {
 
     private static final String EMPTY = "";
 
     public static String refactor(String inVal) {
-        return (inVal == null || inVal.length() == 0)
-                ? EMPTY : inVal.trim();
+        return Optional.ofNullable(inVal).map(String::trim).orElse(EMPTY);
+    }
+
+    public static String nvl(String inVal) {
+        return Optional.ofNullable(inVal).orElse(EMPTY);
     }
 }
